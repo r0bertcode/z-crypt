141f6a72c8c0b929942f2e8676ccdcf816559f95
= <robertlopezca@gmail.com>
2021-07-08 19:41:29 -0700

    Rewrote index functions to take advantage of HMAC-SHA256 alongside AES-256-CBC

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # cryptic-js
-Node.js Library built on-top of Node.js' 'Crypto' module. When using the module myself I found it a bit cumbersome at times and I am providing a cleaner API to access the underlying methods in a more standardized way. 
+Node.js Library built on-top of Node.js' 'Crypto' module. When using the module myself I found it a bit cumbersome at times and I am providing a cleaner API to access the underlying methods in a more standardized way.
+
+
+# Docs
+
+## saltHash (data, [options]) : Hash and salt data
+
+Returns the hashed and salted key ( Uses PBKDF2 )
+```
+  data can be of Type: String, Buffer, TypedArray, DataView
+
+  options => {
+    // Salt needs to be minimum 16 bytes
+    salt: Salt to add to the hash (Default: 116-10016 random bytes)
+
+    // The higher, the more secure, and the longer it will take
+    iters: Number of iters for the hash algo (Default: 100000)
+
+    keyLen: Length of the output key (Default: 64)
+
+    digest: HMAC digest algorithm (Default: 'sha512')
+
+    encoding: encoding of the output (Default: 'hex')
+  }
+
+
+  Usage example:
+
+  const userPassword = 'bestPassword';
+
+  const salted = cryptic.saltHash(userPassword);
+
+  console.log(salted) => a6ec645c5744786b046e7....fb5fb03
+
+  With custom configuration:
+
+  const salted = cryptic.saltHash(userPassword, { iters: 1000000 })
+
+```
+
+<br>
+
+## encryptIv (data, key, [options])
+
+Returns the encrypted data and the IV ( initialization vector )
--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const {
   randomBytes,
   pbkdf2Sync,
   createDecipheriv,
+  getCiphers,
 } = require('crypto');
+
+// Generate a secret key for AES-256-CBC ( Will return a random hex string from 16 bytes )
+const secretKey = () => randomBytes(16).toString('hex');
+// Generate a secret key for AES-192-CCM ( Will return a random hex string from 32 bytes )
+const secretKeyCCM = () => randomBytes(24).toString('hex');
 
 // Salt and hash a String, Buffer, TypedArray, or DataView
 const saltHash = (data, options) => {
   const { salt, iters, keyLen, digest, encoding } = Object.assign({
     // We want the min amount of bytes to be 16 for the salt
-    salt: randomBytes(Math.floor(Math.random() * 10000) + 16),
+    salt: randomBytes(Math.floor(Math.random() * 10000) + 116),
     // Number of iterations for the hash
     iters: 100000,
     // Length of the output key
@@ -23,101 +29,68 @@ const saltHash = (data, options) => {
   return pbkdf2Sync(data, salt, iters, keyLen, digest).toString(encoding);
 };
 
-// Generate a secret key for AES-256-CBC ( Will return a random hex string from 16 bytes )
-const secretKey = () => randomBytes(16).toString('hex');
-// Generate a secret key for AES-192-CCM ( Will return a random hex string from 32 bytes )
-const secretKeyCCM = () => randomBytes(24).toString('hex');
+// AES-256-CBC + HMAC-SHA256 encryption, returning the IV and encrypted string
+const encryptIv = (data, key, inEncoding, outEncoding, iv) => {
+  if (!iv) iv = randomBytes(16);
 
-// Standard AES-256-CBC encryption ( Will return the encrypted string, and the IV )
-const encryptIv = function(data, key, options = {}) {
-  const { encoding, outcoding, iv } = Object.assign({
-    encoding: 'utf-8',
-    outcoding: 'hex',
-    iv: randomBytes(16),
-  }, options);
-
-  const cipher = createCipheriv('aes-256-cbc', key, iv);
-  let encrypted = cipher.update(data, encoding, outcoding);
-  encrypted += cipher.final(outcoding);
+  const cipher = createCipheriv('aes-256-cbc-hmac-sha256', key, iv);
+  let encrypted = cipher.update(data, inEncoding, outEncoding);
+  encrypted += cipher.final(outEncoding);
 
   return { encrypted, iv };
 };
 
-// Standard AES-256-CBC decryption ( Defaults input encoding to HEX as 'encrypt' defaults its outcoding to HEX )
-// Returns the decrypted data
-const decryptIv = function(encrypted, key, iv, options = {}) {
-  const { encoding, outcoding } = Object.assign({
-    encoding: 'hex',
-    outcoding: 'utf-8',
-  }, options);
-
-  const decipher = createDecipheriv('aes-256-cbc', key, iv);
-  let decrypted = decipher.update(encrypted, encoding, outcoding);
-  decrypted += decipher.final(outcoding);
+// AES-256-CBC + HMAC-SHA256 decryption, returning the decrypted string
+const decryptIv = (encrypted, key, iv, inEncoding, outEncoding) => {
+  const decipher = createDecipheriv('aes-256-cbc-hmac-sha256', key, iv);
+  let decrypted = decipher.update(encrypted, inEncoding, outEncoding);
+  decrypted += decipher.final(outEncoding);
 
   return decrypted;
 };
 
-// AES-192-CCM encryption, requiring a Auth tag alongside an (Optional) AAD or String used as an additonal layer of information needed to decrypt
-// This will return the encrypted string, IV, the AuthTag, and AAD
-const encryptCCM = (key, text, encoding, options) => {
-  // aad, iv = randomBytes(12), outcoding = 'hex'
-  let { aad, iv, outcoding, tagLength } = Object.assign({
-    aad: false,
-    iv: randomBytes(12),
-    outcoding: 'hex',
-    tagLength: 16,
-  }, options);
+const key = secretKey();
+const data = 'hello-world';
 
-  if (typeof key === 'string') {
-    key = Buffer.from(key, 'hex');
-  }
+const { encrypted, iv } = encryptIv(data, key, 'utf8', 'hex');
+console.log(encrypted);
+const decrypted = decryptIv(encrypted, key, iv, 'hex', 'utf8');
 
-  const cipher = createCipheriv('aes-192-ccm', key, iv, {
-    authTagLength: tagLength,
-  });
+console.log(decrypted);
 
-  if (aad) {
-    aad = Buffer.from(aad, 'utf-8');
-    cipher.setAAD(aad, {
-      plaintextLength: Buffer.byteLength(text),
-    });
-  }
-
-  let encrypted = cipher.update(text, encoding, outcoding);
-  encrypted += cipher.final(outcoding);
-  const tag = cipher.getAuthTag();
-
-  return { encrypted, iv, tag, aad };
+// AES-256-CBC + HMAC-SHA256 encryption class
+const AES = function(key) {
+  this.key = key;
+  this.iv = randomBytes(16);
 };
 
-// Not working => Error: Unsupported state or unable to authenticate data
-// const decryptCCM = (key, encrypted, iv, tag, outcoding, options) => {
-//   let { aad, encoding, tagLength } = Object.assign({
-//     aad: false,
-//     encoding: 'hex',
-//     tagLength: 16,
-//   }, options);
+// Standard encryption of data, returns the encrypted string
+AES.prototype.encrypt = function(data, inEncoding, outEncoding) {
+  const cipher = createCipheriv('aes-256-cbc-hmac-sha256', this.key, this.iv);
+  let encrypted = cipher.update(data, inEncoding, outEncoding);
+  encrypted += cipher.final(outEncoding);
 
-//   if (typeof key === 'string') {
-//     key = Buffer.from(key, 'hex');
-//   }
+  return encrypted;
+};
 
-//   const decipher = createDecipheriv('aes-192-ccm', key, iv, {
-//     authTagLength: tagLength,
-//   });
-//   decipher.setAuthTag(tag);
+// Standard decryption of a encrypted string, returns the decrypted data
+AES.prototype.decrypt = function(encrypted, inEncoding, outEncoding) {
+  const decipher = createDecipheriv('aes-256-cbc-hmac-sha256', this.key, this.iv);
+  let decrypted = decipher.update(encrypted, inEncoding, outEncoding);
+  decrypted += decipher.final(outEncoding);
 
-//   if (aad) {
-//     aad = Buffer.from(aad, 'utf-8');
-//     decipher.setAAD(aad, {
-//       plaintextLength: encrypted.length,
-//     });
-//   }
+  this.iv = randomBytes(16);
 
-//   let decrypted = decipher.update(encrypted, encoding, outcoding);
-//   decrypted += decipher.final(outcoding);
+  return decrypted;
+};
 
-//   return decrypted;
-// };
+// AES-192-ccm Encryption with CCM mode ( Add a 'AuthTag' and an additonal data authentication (AAD) )
+const AESCCM = function(key) {
+
+};
+
+module.exports = {
+  AES, saltHash, secretKey, secretKeyCCM,
+  encryptIv, decryptIv,
+};
 
