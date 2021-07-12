2295ad7d3f1e9815ecd2e0101ad9a2d7fa925f4a
Robert Lopez <robertlopezca@gmail.com>
2021-07-12 16:16:43 -0700

    Changed order on test for AES to make sure its not in order

--- test/AES.test.js
+++ test/AES.test.js
@@ -6,19 +6,15 @@ const aes = new AES(key);
 
 test('AES encryption / decryption should work', () => {
    const data = JSON.stringify({ test: true });
+   const data2 = secretKey(133);
    
    const encrypted = aes.encrypt(data, 'utf-8', 'hex');
-
-   const decrypted = aes.decrypt(encrypted, 'hex', 'utf-8');
-
-   expect(JSON.parse(decrypted).test).toEqual(true);
-
-   const data2 = secretKey(133);
-
    const encrypted2 = aes.encrypt(data2, 'hex', 'binary');
 
    const decrypted2 = aes.decrypt(encrypted2, 'binary', 'hex');
+   const decrypted = aes.decrypt(encrypted, 'hex', 'utf-8');
 
+   expect(JSON.parse(decrypted).test).toEqual(true);
    expect(decrypted2).toEqual(data2);
 });
 
